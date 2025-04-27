77a53202e270c511bd383dc844e39687e777376f
Nikola Vasiljevic <nvasiljevic6824si@raf.rs>
2025-04-27 02:30:34 +0200

    Changes by nikolanedeljkovic

--- src/main/java/savetovaliste/AppCore.java
+++ src/main/java/savetovaliste/AppCore.java
@@ -1,7 +1,14 @@
 package savetovaliste;
 
+import savetovaliste.core.ApplicationFramework;
+import savetovaliste.core.GUI;
+import savetovaliste.gui.SwingGUI;
+
 public class AppCore {
     public static void main(String[] args) {
-
+        ApplicationFramework appCore = ApplicationFramework.getInstance();
+        GUI swingGui = new SwingGUI();
+        appCore.initialize(swingGui);
+        appCore.run();
     }
 }
