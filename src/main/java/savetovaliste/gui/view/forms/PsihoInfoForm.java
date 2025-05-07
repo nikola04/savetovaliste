467d6b7b0ba14daea5a4032c94d8bc856921249e
Nikola Vasiljevic <nvasiljevic6824si@raf.rs>
2025-05-08 00:59:54 +0200

    fix

--- src/main/java/savetovaliste/gui/view/forms/PsihoInfoForm.java
+++ src/main/java/savetovaliste/gui/view/forms/PsihoInfoForm.java
@@ -1,12 +1,15 @@
 package savetovaliste.gui.view.forms;
 
 import savetovaliste.db.DBUtil;
+import savetovaliste.db.utility.JDBCUtils;
+import savetovaliste.model.Psihoterapeut;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class PsihoInfoForm {
     private JButton refreshBtn;
@@ -23,39 +26,37 @@ public class PsihoInfoForm {
             }
         });
     }
+
+
     public void loadData() {
-        System.out.println("Konekcija uspešna!");
-        try (Connection conn = DBUtil.getConnection()) {
-            System.out.println("Konekcija uspešna!");
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM psihoterapeut");
-
-            DefaultTableModel model = new DefaultTableModel();
-            model.addColumn("ID");
-            model.addColumn("Ime");
-            model.addColumn("Prezime");
-            model.addColumn("JMBG");
-            model.addColumn("Email");
-            model.addColumn("Telefon");
-            model.addColumn("Datum Rodjenja");
-            while (rs.next()) {
-                int id = rs.getInt("psihoterapeut_id");
-                String ime = rs.getString("ime");
-                String prezime = rs.getString("prezime");
-                String jmbg = rs.getString("jmbg");
-                String email = rs.getString("email");
-                String telefon = rs.getString("telefon");
-                Date date = rs.getDate("datum_rodjenja");
-
-                model.addRow(new Object[]{id, ime, prezime, jmbg,email, telefon, date});
-            }psihoTable.setModel(model);
-            rs.close();
-            stmt.close();
+        ArrayList<Psihoterapeut> psihoterapeuts = new ArrayList<>();
+        try {
+            psihoterapeuts.addAll(JDBCUtils.getPsihoterapeuts());
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("ID");
+        model.addColumn("Ime");
+        model.addColumn("Prezime");
+        model.addColumn("JMBG");
+        model.addColumn("Email");
+        model.addColumn("Telefon");
+        model.addColumn("Datum Rodjenja");
+        model.addColumn("Broj Sertifikata");
+        for (Psihoterapeut p : psihoterapeuts) {
+            int id = p.getId();
+            String ime = p.getIme();
+            String prezime = p.getPrezime();
+            String jmbg = p.getJmbg();
+            String email = p.getEmail();
+            String telefon = p.getTelefon();
+            java.util.Date date = p.getDatumRodjenja();
+            int brojSertifikata = p.getSertifikat().getId();
 
-
+            model.addRow(new Object[]{id, ime, prezime, jmbg,email, telefon, date, brojSertifikata});
+        }
+        psihoTable.setModel(model);
     }
 
     public JPanel getPanel1() {
