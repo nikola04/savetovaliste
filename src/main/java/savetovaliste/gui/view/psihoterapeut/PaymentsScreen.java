4a578e9b2291f6b8750628a3c3876df7eb61a8c0
Nikola Nedeljkovic <nikolanedeljkovic.official@gmail.com>
2025-05-08 12:38:32 +0200

    Do pola uradjen Payments

--- src/main/java/savetovaliste/gui/view/psihoterapeut/PaymentsScreen.java
+++ src/main/java/savetovaliste/gui/view/psihoterapeut/PaymentsScreen.java
@@ -4,9 +4,20 @@ import savetovaliste.Session;
 import savetovaliste.controller.observer.ISubscriber;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class PaymentsScreen  extends JPanel implements ISubscriber {
     private static PaymentsScreen instance;
+    private JLabel lblHeading;
+    private JTable klijentiTable;
+    private JScrollPane scrollPane;
+    private DefaultTableModel modelKlijenti;
+    private JButton btnUplate;
+    private JButton btnDugovanja;
+    private JTable uplatedugovanjaTable;
+    private DefaultTableModel modelUplateDugovanja;
+    private JScrollPane scrollPaneUplateDugovanja;
+
 
     public static PaymentsScreen getInstance(){
         if(instance == null) {
@@ -18,6 +29,43 @@ public class PaymentsScreen  extends JPanel implements ISubscriber {
 
     private void initialize() {
         Session.getInstance().addSubscriber(this);
+        lblHeading = new JLabel("Uplate i Dugovanja Klijenata");
+        klijentiTable = new JTable();
+        scrollPane = new JScrollPane(klijentiTable);
+        uplatedugovanjaTable = new JTable();
+        modelKlijenti = new DefaultTableModel();
+        modelUplateDugovanja = new DefaultTableModel();
+        scrollPaneUplateDugovanja = new JScrollPane(uplatedugovanjaTable);
+        btnUplate = new JButton("Uplate Klijenta");
+        btnDugovanja = new JButton("Dugovanja Klijenta");
+        modelKlijenti.addColumn("ID Klijenta");
+        modelKlijenti.addColumn("Ime");
+        modelKlijenti.addColumn("Prezime");
+        modelKlijenti.addColumn("Jmbg");
+        modelKlijenti.addColumn("Email");
+        modelKlijenti.addColumn("Telefon");
+        modelKlijenti.addColumn("Datum rodjenja");
+        modelKlijenti.addColumn("Ranije terapije");
+        modelUplateDugovanja.addColumn("Datum");
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setAlignmentX(CENTER_ALIGNMENT);
+        this.add(lblHeading);
+        this.add(Box.createVerticalStrut(10));
+        this.add(scrollPane);
+        this.add(Box.createVerticalStrut(10));
+        this.add(btnUplate);
+        this.add(Box.createVerticalStrut(10));
+        this.add(btnDugovanja);
+        this.add(Box.createVerticalStrut(10));
+        this.add(new JSeparator(SwingConstants.HORIZONTAL));
+        this.add(scrollPaneUplateDugovanja);
+
+        setTables();
+
+    }
+
+    private void setTables() {
     }
 
     @Override
