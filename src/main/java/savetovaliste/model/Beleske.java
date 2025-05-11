381047ad06fb6244bcdf2e6326a92a6bc6050c83
Nikola Nedeljkovic <nikolanedeljkovic.official@gmail.com>
2025-05-11 13:16:57 +0200

    Added Beleske model

--- src/main/java/savetovaliste/model/Beleske.java
+++ src/main/java/savetovaliste/model/Beleske.java
@@ -1,4 +1,21 @@
 package savetovaliste.model;
 
 public class Beleske {
+    private final int id;
+    private final String text;
+    private final Seansa seansa;
+    public Beleske(int id, String text, Seansa seansa) {
+        this.id = id;
+        this.text = text;
+        this.seansa = seansa;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getText() {
+        return text;
+    }
+    public Seansa getSeansa() {
+        return seansa;
+    }
 }
