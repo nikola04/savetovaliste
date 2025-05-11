a478b9014866a4b4ad70ac9a23dc7b746fc54d69
Nikola Nedeljkovic <nikolanedeljkovic.official@gmail.com>
2025-05-11 12:43:48 +0200

    Updated neplaceno class

--- src/main/java/savetovaliste/model/Neplaceno.java
+++ src/main/java/savetovaliste/model/Neplaceno.java
@@ -4,12 +4,16 @@ public class Neplaceno {
     private final int id; // seansaId ili testiranjeId zavisi od tip
     private final String tip; // seansa ili testiranje
     private final double iznos;
+    private final boolean naRate;
+    private final boolean placeno;
     private final boolean istekaoRok; // ako je na rate i istekao je rok / ako nije na rate i nikada nije uplacena prva rata bice false
-    public Neplaceno(int id, String tip, double iznos, boolean istekaoRok) {
+    public Neplaceno(int id, String tip, double iznos, boolean placeno, boolean naRate, boolean istekaoRok) {
         this.id = id;
         this.tip = tip;
         this.iznos = iznos;
         this.istekaoRok = istekaoRok;
+        this.placeno = placeno;
+        this.naRate = naRate;
     }
 
     public int getId() {
